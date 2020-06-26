7737af54522c74a4d18ea6b9c21100c2e257155a
Song Zhao <song.zhao@amlogic.com>
2020-06-25 23:41:30 -0700

    h264_dec: config buf specs after resolution change [1/1]
    PD#SWPL-28501
    
    Problem:
    In V4L2 mode, resolution change will trigger reset() and all the
    canvas are released with h264_reconfig(). When it continues decoding
    the data with new resolution, decoder will write to invalid canvas.
    
    Solution:
    In vh264_set_params() call config_buf_specs in V4L2 mode.
    
    Verify:
    U212
    gst-play-1.0 http://www.bok.net/dash/tears_of_steel/cleartext/stream.mpd
    
    Change-Id: Ia57678e91001d719b9477a62fcae7688455decfd
    Signed-off-by: Song Zhao <song.zhao@amlogic.com>

--- drivers/amvdec_ports/aml_vcodec_dec.c
+++ drivers/amvdec_ports/aml_vcodec_dec.c
@@ -604,6 +604,7 @@ void trans_vframe_to_user(struct aml_vcodec_ctx *ctx, struct vdec_v4l2_buffer *f
 			if (dstbuf->frame_buffer.num_planes == 2)
 				kernel_write(fp,vb2_plane_vaddr(vb, 1),
 						vb->planes[1].length, 0);
+			pr_info("dump idx: %d %dx%d\n", dump_capture_frame, vf->width, vf->height);
 			dump_capture_frame--;
 			filp_close(fp, NULL);
 		}
