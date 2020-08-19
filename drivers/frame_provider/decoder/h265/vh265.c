14408a0478881c732bb9e198636055800fd44ca8
yao liu <yao.liu@amlogic.com>
2020-08-19 22:06:47 +0800

    decoder: flash screen when play some dv stream [1/1]
    PD#SWPL-31990
    
    Problem:
    decoder send vframe without sei
    
    Solution:
    disable update_vframe_src_fmt, get aux data through
    interface VFRAME_EVENT_RECEIVER_GET_AUX_DATA
    
    Verify:
    tm2 ab301
    
    Change-Id: Iec25d53d3db1d977a3eb8d99d4038ef9ead2d442
    Signed-off-by: yao liu <yao.liu@amlogic.com>

--- drivers/frame_provider/decoder/h265/vh265.c
+++ drivers/frame_provider/decoder/h265/vh265.c
@@ -9360,10 +9360,6 @@ static int prepare_display_buf(struct hevc_state_s *hevc, struct PIC_s *pic)
 			get_pic_poc(hevc, (vf->index >> 8) & 0xff),
 			vf->pts, vf->pts_us64,
 			vf->duration);
-		update_vframe_src_fmt(vf,
-			hevc->m_PIC[vf->index & 0xff]->aux_data_buf,
-			hevc->m_PIC[vf->index & 0xff]->aux_data_size,
-			false, hevc->provider_name, NULL);
 
 		/*if (pic->vf_ref == hevc->vf_pre_count) {*/
 		if (hevc->kpi_first_i_decoded == 0) {
