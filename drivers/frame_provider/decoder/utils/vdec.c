8435acb7d840a1a8f0b0c32afef5fc19be7c4aec
Nanxin Qin <nanxin.qin@amlogic.com>
2020-09-09 17:01:27 +0800

    vmh264: add the feature of fence for h264 decoder. [2/2]
    PD#SWPL-32845
    
    Problem:
    need decode fence  on cloud game docking
    
    Solution:
    1. add the feature of fence for h264 decoder.
    2. just used ip only stream source.
    
    Verify:
    u212
    
    Change-Id: I2ff8cab7635f49702facf943fc51635784264265
    Signed-off-by: Nanxin Qin <nanxin.qin@amlogic.com>

--- drivers/frame_provider/decoder/utils/vdec.c
+++ drivers/frame_provider/decoder/utils/vdec.c
@@ -2495,6 +2495,13 @@ s32 vdec_init(struct vdec_s *vdec, int is_4k)
 				vdec->vf_receiver_name);
 			snprintf(vdec->vfm_map_id, VDEC_MAP_NAME_SIZE,
 				"vdec-map-%d", vdec->id);
+		} else if (p->frame_base_video_path ==
+			FRAME_BASE_PATH_AMLVIDEO_FENCE) {
+			snprintf(vdec->vfm_map_chain, VDEC_MAP_NAME_SIZE,
+				"%s %s", vdec->vf_provider_name,
+				"amlvideo amvideo");
+			snprintf(vdec->vfm_map_id, VDEC_MAP_NAME_SIZE,
+				"vdec-map-%d", vdec->id);
 		}
 
 		if (vfm_map_add(vdec->vfm_map_id,
