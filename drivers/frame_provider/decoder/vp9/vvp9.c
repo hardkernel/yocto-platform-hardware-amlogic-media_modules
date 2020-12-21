6accce929ae49795f6e36d17fa27981405f19511
gan.zhang <gan.zhang@amlogic.com>
2020-12-21 20:00:25 +0800

    vp9: fix hdr10 case test fail. [3/3]
    PD#SWPL-34209
    
    Problem:
    Transmitting the wrong luminance value to the upper layer.
    
    Solution:
    Pass the correct HDR data to the upper layer.
    
    Verify:
    AH212
    
    Change-Id: Ia05b8b38bafbcf9fdd2ff9f7964f799cc44c3c1c
    Signed-off-by: gan.zhang <gan.zhang@amlogic.com>

--- drivers/frame_provider/decoder/vp9/vvp9.c
+++ drivers/frame_provider/decoder/vp9/vvp9.c
@@ -7173,6 +7173,9 @@ static void set_frame_info(struct VP9Decoder_s *pbi, struct vframe_s *vf)
 		memset(&hdr, 0, sizeof(hdr));
 		hdr.signal_type = vf->signal_type;
 		hdr.color_parms = pbi->vf_dp;
+
+		hdr.color_parms.luminance[0] = hdr.color_parms.luminance[0] / 10000;
+
 		vdec_v4l_set_hdr_infos(ctx, &hdr);
 	}
 
