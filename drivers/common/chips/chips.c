f8e433f83a10c16014f782574642d5f218dec104
zehong.luo <zehong.luo@amlogic.com>
2025-03-12 13:38:33 +0800

    gxlx4: CF1 decoder bringup. [1/1]
    PD#SWPL-205493
    
    Problem:
    gxlx4 decoder bringup.
    
    Solution:
    add gxlx4 decoder features code.
    
    Verify:
    t950d5
    
    SourceCode:
    ucode:
    0.4.183-gf5e1a6a
    I2131b
    I25050
    I762bf
    Id7df5
    I14824
    
    Change-Id: Id68ecb28e0facd021d4701cfe031175771dcdc77
    Signed-off-by: zehong.luo <zehong.luo@amlogic.com>

--- drivers/common/chips/chips.c
+++ drivers/common/chips/chips.c
@@ -110,6 +110,7 @@ static int codec_level_idc_init(int format)
 		{AM_MESON_CPU_MAJOR_ID_S7D,   0,   5,   51,  0,    0,    0,   0,   0,   0,   51,  51,  51,  0,      0,     0,   0,    51,  0,    0},
 		{AM_MESON_CPU_MAJOR_ID_S6,    0,   5,   52,  0,    0,    0,   0,   0,   0,   52,  52,  52,  0,      0,     0,   0,    52,  0,    52},
 		{AM_MESON_CPU_MAJOR_ID_T6D,   0,   5,   42,  0,    0,    0,   0,   0,   0,   42,  42,  41,  0,      0,     0,   0,    41,  0,    0},
+		{AM_MESON_CPU_MAJOR_ID_GXLX4, 0,   5,   51,  0,    0,    0,   0,   0,   0,   51,  51,  51,  0,      0,     0,   0,     0,  0,    0},
 	};
 	const u32 sub_chip_level[][1 + VFORMAT_MAX] = {
 		/* chip,                          mp2, mp4, 264, mjpg, real, jpg, vc1, avs, yuv, mvc, 2k4k, 265, 264enc, jpenc, vp9, avs2, av1, avs3, vvc*/
