162ecc7536b9dc286cfd6cdd3842721ea14d45ee
xing.xu <xing.xu@amlogic.com>
2025-05-06 16:48:31 +0800

    scripts: CB2 version_control.sh reports errors with git commands [1/1]
    PD#SWPL-212511
    
    Problem:
    Unable to fetch git commands during compilation
    
    Solution:
    Using absolute paths with git
    
    Verify:
    local
    
    Change-Id: Id7148b39114aa236ddf4455c2fccde28a908bcd3
    Signed-off-by: xing.xu <xing.xu@amlogic.com>

--- version_control.sh
+++ version_control.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+GIT_BIN="/usr/bin/git"
 
 MEDIA_MODULE_PATH=$(cd "$(dirname "$0")";pwd)
 Major_V=$(cd ${MEDIA_MODULE_PATH}/; grep "Major_V" VERSION  | awk -F [=] '{print $2}')
 Minor_V=$(cd ${MEDIA_MODULE_PATH}/; grep "Minor_V" VERSION  | awk -F [=] '{print $2}')
 BASE_CHANGEID=$(cd ${MEDIA_MODULE_PATH}/; grep "^DevelopingChangeId" VERSION | awk -F [=] '{print $2}' | cut -c1-6)
-#MEDIAMODULE_CHANGEID=$(cd ${MEDIA_MODULE_PATH}; git log -1 ${MEDIA_MODULE_PATH} | grep "Change-Id: " | awk '{ print $2}' | cut -c1-6 | tail -1)
-COMMIT_COUNT=$(cd ${MEDIA_MODULE_PATH}/; git log | grep "Change-Id: " | grep -n ${BASE_CHANGEID} | awk -F ":" '{printf "%d", $1-1}' )
-MEDIAMODULE_COMMITID=$(cd ${MEDIA_MODULE_PATH}/; git rev-parse --short HEAD)
+#MEDIAMODULE_CHANGEID=$(cd ${MEDIA_MODULE_PATH}; $GIT_BIN log -1 ${MEDIA_MODULE_PATH} | grep "Change-Id: " | awk '{ print $2}' | cut -c1-6 | tail -1)
+COMMIT_COUNT=$(cd ${MEDIA_MODULE_PATH}/; $GIT_BIN log | grep "Change-Id: " | grep -n ${BASE_CHANGEID} | awk -F ":" '{printf "%d", $1-1}' )
+MEDIAMODULE_COMMITID=$(cd ${MEDIA_MODULE_PATH}/; $GIT_BIN rev-parse --short HEAD)
 UCODE_VERSION_DETAIL=$(cd ${MEDIA_MODULE_PATH}/; ./firmware/checkmsg ./firmware/video_ucode.bin | grep "ver    :" | awk '{print $3}' | sed 's/v//g')
 UCODE_VERSION=$(cd ${MEDIA_MODULE_PATH}/; ./firmware/checkmsg ./firmware/video_ucode.bin | grep "ver    :"  | awk -F '[v-]' '{print $3}' | awk -F [\.] '{printf "%d%02d%03d", $1,$2,$3}')
 RELEASED_VERSION=$(cd ${MEDIA_MODULE_PATH}/; grep "^#V" VERSION | head -1 | awk  '{print $1}' | sed 's/#//g')
