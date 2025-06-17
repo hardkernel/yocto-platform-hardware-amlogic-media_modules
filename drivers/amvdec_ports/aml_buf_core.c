c97a3a5f742e56a5f63be86934fae2fa45e30a2d
miaohong chen <miaohong.chen@amlogic.com>
2025-06-17 14:41:21 +0800

    v4ldec: CB0 Kernel crash when accessing workqueue. [1/1]
    PD#SWPL-221916
    
    Problem:
    The workqueue failed to be created when the broadcast started,
    and when it was exited,
    it flushed the queue, causing a null pointer to crash.
    
    Solution:
    When a v4ldec instance exits, it checks if the workqueue exists.
    
    Verify:
    ohm
    
    Change-Id: Ia24f6c1adede8d2559a0e6d97a96be6e8a63d000
    Signed-off-by: miaohong chen <miaohong.chen@amlogic.com>
    (cherry picked from commit b6a949de72bbfb214b1f4d28d5f0f85185ba8060)

--- drivers/amvdec_ports/aml_buf_core.c
+++ drivers/amvdec_ports/aml_buf_core.c
@@ -1174,8 +1174,10 @@ static void buf_core_reset(struct buf_core_mgr_s *bc, bool v4l_res_change)
 		bc->vpp_reset(bc, v4l_res_change);
 
 	mutex_lock(&bc->workqueue_mutex);
-	flush_workqueue(bc->recycle_buf_ref_workqueue);
-	bc->workqueue_enabled = false;
+	if (bc->recycle_buf_ref_workqueue) {
+		flush_workqueue(bc->recycle_buf_ref_workqueue);
+		bc->workqueue_enabled = false;
+	}
 	mutex_unlock(&bc->workqueue_mutex);
 
 	mutex_lock(&bc->mutex);
@@ -1783,9 +1785,11 @@ void buf_core_mgr_release(struct buf_core_mgr_s *bc)
 	v4l_dbg_ext(bc->id, V4L_DEBUG_CODEC_BUFMGR, "%s\n", __func__);
 
 	mutex_lock(&bc->workqueue_mutex);
-	flush_workqueue(bc->recycle_buf_ref_workqueue);
-	destroy_workqueue(bc->recycle_buf_ref_workqueue);
-	bc->workqueue_enabled = false;
+	if (bc->recycle_buf_ref_workqueue) {
+		flush_workqueue(bc->recycle_buf_ref_workqueue);
+		destroy_workqueue(bc->recycle_buf_ref_workqueue);
+		bc->workqueue_enabled = false;
+	}
 	mutex_unlock(&bc->workqueue_mutex);
 
 	kref_put(&bc->core_ref, buf_core_destroy);
